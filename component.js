fd028302bd54bf4700dc9a550b81216d4911113d
Gael Santos <jaffary21gball360@gmail.com>
2026-02-07 22:30:05 -0500

    Added navbar and footer, as well as two new pages and fonts

---
+++ component.js
@@ -0,0 +1,38 @@
+class MyNavbar extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <div class="navbar fira-sans-medium">
+            <a href="index.html"><img src="images/signature.png"></a>
+            <nav class="nav">
+                <h5 class="option">
+                    <a class="black-text" href="index.html">Home</a>
+                </h5>
+                <h5 class="option">
+                    <a class="black-text" href="portfolio.html">Portfolio</a>
+                </h5>
+                <h5 class="option">
+                    <a class="black-text" href="resume.html">Resume</a>
+                </h5>
+            </nav>
+        </div>
+        `
+    }
+}
+
+class MyFooter extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <footer>
+            <p class="black-text fira-sans-medium">Footer text</p>
+            <div class="socials">
+                <a href="index.html" target="_blank"><img src="images/instagram2.png"></a>
+                <a href="index.html" target="_blank"><img src="images/github.png"></a>
+                <a href="index.html" target="_blank"><img src="images/linkedin.png"></a>
+            </div>
+        </footer>
+        `
+    }
+}
+
+customElements.define("my-navbar", MyNavbar);
+customElements.define("my-footer", MyFooter);
